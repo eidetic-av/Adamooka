d7485c6e2ebc0975cc4839d44409ac96bed4b5c3
Matthew Hughes <matt.hughes@live.com>
2018-06-20 22:27:15 +1000

    Started creating OSC Addresses from TargetComponents in OscAddressRegistrar

--- Assets/Scripts/Osc/OscAddressRegistrar.cs
+++ Assets/Scripts/Osc/OscAddressRegistrar.cs
@@ -8,12 +8,32 @@ using UnityEngine;
 public class OscAddressRegistrar : MonoBehaviour
 {
     public List<Component> TargetComponents;
-    
+
+    string RootAddress { get; set; }
+    List<string> ComponentAddresses { get; set; } = new List<string>();
+
     void Start()
     {
+        // Set the root address ("BaseAddress/.../...")
+        // to the name of the GameObject this script is attached to (without spaces)
+        RootAddress = gameObject.name.Replace(" ", "");
+
         // For every component added to the public TargetComponents list
         TargetComponents.ForEach(targetComponent =>
         {
+            // Set the address of each component to its Component type name...
+            // Add it to the ComponentAddresses list so that we don't double up
+            // If there are multiple, add an index to the address
+            // e.g. the second ParticleSystem on an object will be addressed as "RootAddress/ParticleSystem1"
+            var componentTypeIndex = 0;
+            var componentAddress = RootAddress + "/" + targetComponent.GetType().Name;
+            while (ComponentAddresses.Contains(componentAddress))
+            {
+                componentTypeIndex++;
+                componentAddress = RootAddress + "/" + targetComponent.GetType().Name + componentTypeIndex;
+            }
+            ComponentAddresses.Add(componentAddress);
+
             // Find the Type, and get all the publicly accesible properties of the Type
             Type targetComponentType = targetComponent.GetType();
             PropertyInfo[] componentProperties = targetComponentType.GetProperties();
@@ -27,7 +47,7 @@ public class OscAddressRegistrar : MonoBehaviour
                 if (property.Name == "emission")
                 {
                     var emissionProperties = property.PropertyType.GetProperties();
-                    emissionProperties.ToList().ForEach(p => Debug.Log(p.Name));
+                    //emissionProperties.ToList().ForEach(p => Debug.Log(p.Name));
                 }
 
             });
---
+++ Assets/Scripts/Osc/OscRouter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscRouter : MonoBehaviour {
+
+    public static OscRouter Instance;
+
+    public string TestPropertyAddress = "";
+    public Boolean SetTestProperty = false;
+    
+    /// <summary>
+    /// Dictionary containing the methods to invoke when attempting to set Component properties.
+    /// They key is the OSC Address as a string; the value is the setter method as an Action.
+    /// </summary>
+    public Dictionary<string, Action> PropertySetters { get; set; } = new Dictionary<string, Action>();
+    
+	void Start () {
+        if (Instance != null)
+            throw (new UnityException("Only use one OscRouter in a scene!"));
+        Instance = this;
+	}
+	
+	void Update () {
+		if (SetTestProperty)
+        {
+
+        }
+	}
+}
