80570be6fce4dd2473eabe855621153fe4bb10c2
Matthew Hughes <matt.hughes@live.com>
2018-06-20 21:19:47 +1000

    Started Reflection-based OSC Manipulation tools

---
+++ Assets/Scripts/Osc/OscAddressRegistrar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class OscAddressRegistrar : MonoBehaviour
+{
+    public List<Component> TargetComponents;
+    
+    void Start()
+    {
+        // For every component added to the public TargetComponents list
+        TargetComponents.ForEach(targetComponent =>
+        {
+            // Find the Type, and get all the publicly accesible properties of the Type
+            Type targetComponentType = targetComponent.GetType();
+            PropertyInfo[] componentProperties = targetComponentType.GetProperties();
+
+            componentProperties.ToList().ForEach(property =>
+            {
+                var isComponent = property.PropertyType.IsSubclassOf(typeof(Component));
+                var isClass = property.PropertyType.IsClass;
+                var isStruct = (property.PropertyType.IsValueType && !property.PropertyType.IsPrimitive && !property.PropertyType.IsEnum);
+
+                if (property.Name == "emission")
+                {
+                    var emissionProperties = property.PropertyType.GetProperties();
+                    emissionProperties.ToList().ForEach(p => Debug.Log(p.Name));
+                }
+
+            });
+        });
+    }
+}
