7c28197c14dcab91e9840ffb3c3e985e032127ea
Matthew Hughes <matt.hughes@live.com>
2018-06-21 01:56:46 +1000

    last commit before bed...

--- Assets/Scripts/Osc/OscAddressRegistrar.cs
+++ Assets/Scripts/Osc/OscAddressRegistrar.cs
@@ -9,14 +9,15 @@ using UnityEngine;
 public class OscAddressRegistrar : MonoBehaviour
 {
     public List<Component> TargetComponents;
-    
-    string RootAddress { get; set; }
+
     List<string> ComponentAddresses { get; set; } = new List<string>();
+
+    string RootAddress { get; set; }
 
     /// <summary>
     /// Members of Types in this list shouldn't be given an OSC address
     /// </summary>
-    readonly List<Type> IgnoreTypes = new List<Type>()
+    readonly List<Type> IgnoreMemberTypes = new List<Type>()
     {
         typeof(GameObject),
         typeof(HideFlags)
@@ -28,45 +29,46 @@ public class OscAddressRegistrar : MonoBehaviour
         // to the name of the GameObject this script is attached to (without spaces)
         RootAddress = gameObject.name.Replace(" ", "");
         // then, run initialisation asynchronously since it might take a while
-        Task.Run(() => RegisterAddresses());
+        //Task.Run(() => RegisterAddresses());
+        RegisterAddresses();
     }
 
     void RegisterAddresses()
     {
         // For every component added to the public TargetComponents list
-        TargetComponents.ForEach(targetComponent =>
+        TargetComponents.ForEach(targetComponent => RegisterComponentAdresses(targetComponent, targetComponent.GetType().Name, RootAddress));
+    }
+
+    void RegisterComponentAdresses(Component targetComponent, string targetComponentName, string parentAddress)
+    {
+        // Set the address of each component to its Component type name...
+        // Add it to the ComponentAddresses list so that we don't double up
+        var componentTypeIndex = 0;
+        var componentAddress = parentAddress + "/" + targetComponentName;
+
+        while (ComponentAddresses.Contains(componentAddress))
         {
-            // Set the address of each component to its Component type name...
-            // Add it to the ComponentAddresses list so that we don't double up
-            // If there are multiple, add an index to the address
-            // e.g. the second ParticleSystem on an object will be addressed as "RootAddress/ParticleSystem1"
-            var componentTypeIndex = 0;
-            var componentAddress = RootAddress + "/" + targetComponent.GetType().Name;
+            componentTypeIndex++;
+            componentAddress = parentAddress + "/" + targetComponentName + componentTypeIndex;
+        }
+        ComponentAddresses.Add(componentAddress);
 
-            while (ComponentAddresses.Contains(componentAddress))
-            {
-                componentTypeIndex++;
-                componentAddress = RootAddress + "/" + targetComponent.GetType().Name + componentTypeIndex;
-            }
-            ComponentAddresses.Add(componentAddress);
+        // Find the Type, and get all the publicly accesible members of the Type
+        Type targetComponentType = targetComponent.GetType();
+        MemberInfo[] componentMembers = targetComponentType.GetMembers();
 
-            // Find the Type, and get all the publicly accesible members of the Type
-            Type targetComponentType = targetComponent.GetType();
-            MemberInfo[] componentMembers = targetComponentType.GetMembers();
+        componentMembers.ToList().ForEach(member =>
+        {
+            var isProperty = (targetComponentType.GetProperty(member.Name) != null);
+            var isField = (targetComponentType.GetField(member.Name) != null);
+            // only address properties and fields... ignore methods, enums etc.
+            if (!isProperty && !isField) return;
 
-            componentMembers.ToList().ForEach(member =>
-            {
-                var isProperty = (targetComponentType.GetProperty(member.Name) != null);
-                var isField = (targetComponentType.GetField(member.Name) != null);
-                // only address properties and fields... ignore methods, enums etc.
-                if (!isProperty && !isField) return;
-
-                RegisterComponentMemberAddress(targetComponent, targetComponentType, member, componentAddress, isProperty);
-            });
+            RegisterObjectMemberAddress(targetComponent, targetComponentType, member, componentAddress, isProperty);
         });
     }
 
-    void RegisterComponentMemberAddress(Component component, Type componentType, MemberInfo member, string parentAddress, bool isProperty)
+    void RegisterObjectMemberAddress(object targetObject, Type targetObjectType, MemberInfo member, string parentAddress, bool isProperty)
     {
         var memberAddress = parentAddress + "/" + member.Name;
 
@@ -78,7 +80,7 @@ public class OscAddressRegistrar : MonoBehaviour
 
         if (isProperty)
         {
-            property = (componentType.GetProperty(member.Name));
+            property = (targetObjectType.GetProperty(member.Name));
             memberType = property.PropertyType;
 
             isComponent = memberType.IsSubclassOf(typeof(Component));
@@ -88,7 +90,7 @@ public class OscAddressRegistrar : MonoBehaviour
         else
         {
             // if it's not a property it's a field
-            field = (componentType.GetField(member.Name));
+            field = (targetObjectType.GetField(member.Name));
             memberType = field.FieldType;
 
             isComponent = memberType.IsSubclassOf(typeof(Component));
@@ -97,7 +99,7 @@ public class OscAddressRegistrar : MonoBehaviour
         }
 
         // Ignore any members whose types are in the IgnoreTypes list
-        if (IgnoreTypes.Contains(memberType)) return;
+        if (IgnoreMemberTypes.Contains(memberType)) return;
 
         // if the type isn't a primitive value type,
         // we need to add addresses to it's own members recursively
@@ -113,7 +115,7 @@ public class OscAddressRegistrar : MonoBehaviour
                 // create Property setter Action and register it with the OscRouter
                 OscRouter.Instance.MemberSetters.Add(memberAddress, (object value) =>
                 {
-                    property.SetValue(component, value);
+                    property.SetValue(targetObject, value);
                 });
             }
             else
@@ -121,14 +123,46 @@ public class OscAddressRegistrar : MonoBehaviour
                 // create Field setter Action and register it with the OscRouter
                 OscRouter.Instance.MemberSetters.Add(memberAddress, (object value) =>
                 {
-                    field.SetValue(component, value);
+                    field.SetValue(targetObject, value);
                 });
             }
-            Debug.Log(memberAddress + "\n" + memberType.Name);
+            Debug.Log(memberAddress + "\n");
         }
         else
         {
+            // TODO: figure out child members!!!
 
-        }
+            var addressValues = memberAddress.Split('/');
+            var lastAddressValue = addressValues[addressValues.Length - 2];
+
+            // if the lastAddressValue is the same as the name of this member,
+            // it probably refers to itself.
+            // Skip it so we dont recurse infinitely
+
+            // e.g. ParticleSystem contains a member called particleSystem,
+            // which refers to itself -
+            // so it would loop through particleSystem.particleSystem.particleSystem...
+
+            if (lastAddressValue == member.Name) return;
+
+            MemberInfo[] childMembers = memberType.GetMembers();
+
+            childMembers.ToList().ForEach(childMember =>
+            {
+                var childMemberIsProperty = (memberType.GetProperty(childMember.Name) != null);
+                var childMemberIsField = (memberType.GetField(childMember.Name) != null);
+                // only address properties and fields... ignore methods, enums etc.
+                if (!childMemberIsProperty && !childMemberIsField) return;
+
+                if (childMemberIsProperty)
+                {
+                    //var memberObject = targetObjectType.GetProperty(member.Name).GetValue(targetObject);
+                    //var memberObjectType = memberObject.GetType();
+
+                    //RegisterObjectMemberAddress(memberObject, memberObjectType, childMember, memberAddress, childMemberIsProperty);
+                }
+            });
+
+    }
     }
 }
