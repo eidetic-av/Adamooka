483e062ecf07d07787dbc18b3ad0f4d2edc5625e
Matthew Hughes <matt.hughes@live.com>
2018-06-21 00:31:00 +1000

    OSC Address generation for primitives and setter actions working

--- Assets/Scripts/Osc/OscAddressRegistrar.cs
+++ Assets/Scripts/Osc/OscAddressRegistrar.cs
@@ -3,21 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class OscAddressRegistrar : MonoBehaviour
 {
     public List<Component> TargetComponents;
-
+    
     string RootAddress { get; set; }
     List<string> ComponentAddresses { get; set; } = new List<string>();
+
+    /// <summary>
+    /// Members of Types in this list shouldn't be given an OSC address
+    /// </summary>
+    readonly List<Type> IgnoreTypes = new List<Type>()
+    {
+        typeof(GameObject),
+        typeof(HideFlags)
+    };
 
     void Start()
     {
         // Set the root address ("BaseAddress/.../...")
         // to the name of the GameObject this script is attached to (without spaces)
         RootAddress = gameObject.name.Replace(" ", "");
+        // then, run initialisation asynchronously since it might take a while
+        Task.Run(() => RegisterAddresses());
+    }
 
+    void RegisterAddresses()
+    {
         // For every component added to the public TargetComponents list
         TargetComponents.ForEach(targetComponent =>
         {
@@ -27,6 +42,7 @@ public class OscAddressRegistrar : MonoBehaviour
             // e.g. the second ParticleSystem on an object will be addressed as "RootAddress/ParticleSystem1"
             var componentTypeIndex = 0;
             var componentAddress = RootAddress + "/" + targetComponent.GetType().Name;
+
             while (ComponentAddresses.Contains(componentAddress))
             {
                 componentTypeIndex++;
@@ -34,23 +50,85 @@ public class OscAddressRegistrar : MonoBehaviour
             }
             ComponentAddresses.Add(componentAddress);
 
-            // Find the Type, and get all the publicly accesible properties of the Type
+            // Find the Type, and get all the publicly accesible members of the Type
             Type targetComponentType = targetComponent.GetType();
-            PropertyInfo[] componentProperties = targetComponentType.GetProperties();
+            MemberInfo[] componentMembers = targetComponentType.GetMembers();
 
-            componentProperties.ToList().ForEach(property =>
+            componentMembers.ToList().ForEach(member =>
             {
-                var isComponent = property.PropertyType.IsSubclassOf(typeof(Component));
-                var isClass = property.PropertyType.IsClass;
-                var isStruct = (property.PropertyType.IsValueType && !property.PropertyType.IsPrimitive && !property.PropertyType.IsEnum);
+                var isProperty = (targetComponentType.GetProperty(member.Name) != null);
+                var isField = (targetComponentType.GetField(member.Name) != null);
+                // only address properties and fields... ignore methods, enums etc.
+                if (!isProperty && !isField) return;
 
-                if (property.Name == "emission")
-                {
-                    var emissionProperties = property.PropertyType.GetProperties();
-                    //emissionProperties.ToList().ForEach(p => Debug.Log(p.Name));
-                }
-
+                RegisterComponentMemberAddress(targetComponent, targetComponentType, member, componentAddress, isProperty);
             });
         });
+    }
+
+    void RegisterComponentMemberAddress(Component component, Type componentType, MemberInfo member, string parentAddress, bool isProperty)
+    {
+        var memberAddress = parentAddress + "/" + member.Name;
+
+        bool isComponent, isClass, isStruct;
+        Type memberType;
+
+        PropertyInfo property = null;
+        FieldInfo field = null;
+
+        if (isProperty)
+        {
+            property = (componentType.GetProperty(member.Name));
+            memberType = property.PropertyType;
+
+            isComponent = memberType.IsSubclassOf(typeof(Component));
+            isClass = memberType.IsClass;
+            isStruct = (memberType.IsValueType && !memberType.IsPrimitive && !memberType.IsEnum);
+        }
+        else
+        {
+            // if it's not a property it's a field
+            field = (componentType.GetField(member.Name));
+            memberType = field.FieldType;
+
+            isComponent = memberType.IsSubclassOf(typeof(Component));
+            isClass = memberType.IsClass;
+            isStruct = (memberType.IsValueType && !memberType.IsPrimitive && !memberType.IsEnum);
+        }
+
+        // Ignore any members whose types are in the IgnoreTypes list
+        if (IgnoreTypes.Contains(memberType)) return;
+
+        // if the type isn't a primitive value type,
+        // we need to add addresses to it's own members recursively
+        bool isPrimitive = !(isComponent || isClass || isStruct);
+        // if it's a String (as opposed to a string), also treat it as a primitive
+        if (memberType == typeof(String)) isPrimitive = true;
+
+        if (isPrimitive)
+        {
+            // if it is a primitive, register an OSC address for it
+            if (isProperty)
+            {
+                // create Property setter Action and register it with the OscRouter
+                OscRouter.Instance.MemberSetters.Add(memberAddress, (object value) =>
+                {
+                    property.SetValue(component, value);
+                });
+            }
+            else
+            {
+                // create Field setter Action and register it with the OscRouter
+                OscRouter.Instance.MemberSetters.Add(memberAddress, (object value) =>
+                {
+                    field.SetValue(component, value);
+                });
+            }
+            Debug.Log(memberAddress + "\n" + memberType.Name);
+        }
+        else
+        {
+
+        }
     }
 }
--- Assets/Scripts/Osc/OscRouter.cs
+++ Assets/Scripts/Osc/OscRouter.cs
@@ -3,29 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OscRouter : MonoBehaviour {
+public class OscRouter : MonoBehaviour
+{
 
     public static OscRouter Instance;
 
     public string TestPropertyAddress = "";
+    public float TestPropertyValue = 5.0f;
     public Boolean SetTestProperty = false;
-    
+
     /// <summary>
-    /// Dictionary containing the methods to invoke when attempting to set Component properties.
+    /// Dictionary containing the methods to invoke when attempting to set Component properties or fields.
     /// They key is the OSC Address as a string; the value is the setter method as an Action.
     /// </summary>
-    public Dictionary<string, Action> PropertySetters { get; set; } = new Dictionary<string, Action>();
-    
-	void Start () {
+    public Dictionary<string, Action<object>> MemberSetters = new Dictionary<string, Action<object>>();
+
+    void Start()
+    {
         if (Instance != null)
             throw (new UnityException("Only use one OscRouter in a scene!"));
         Instance = this;
-	}
-	
-	void Update () {
-		if (SetTestProperty)
-        {
+    }
 
+    void Update()
+    {
+        if (SetTestProperty)
+        {
+            if (MemberSetters.ContainsKey(TestPropertyAddress))
+            {
+                MemberSetters[TestPropertyAddress].Invoke(TestPropertyValue);
+            }
+            SetTestProperty = false;
         }
-	}
+    }
 }
