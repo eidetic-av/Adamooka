09899550b87a043f3319371da2ba38d0d3b168ea
Matthew Hughes <matt.hughes@live.com>
2018-07-23 11:52:29 +1000

    NoiseCircleController updates

--- Assets/NoiseCircleController.cs
+++ Assets/NoiseCircleController.cs
@@ -10,30 +10,32 @@ public class NoiseCircleController : MonoBehaviour
     public float Radius = 1.5f;
 
     public bool Bang;
-    public Vector2Int IndicesRange = new Vector2Int(30, 50);
-    private Vector2 ScalePosition = Vector2.one;
-    public float ScaleMultiplier = 1f;
+    private Vector2 CurveIntensity = Vector2.zero;
+    public float IntensityMultiplier = 0f;
     public float BangDamping = 5f;
 
     public AnimationCurve WeightCurve;
 
+    public bool CurveNoise = true;
+    public float CurveNoiseIntensity = 1f;
+    private float[] CurveNoiseValues;
+
     void Update()
     {
-        if (Bang)
-        {
-            ScalePosition.y = ScaleMultiplier;
-            Bang = false;
-        }
-
-        if (ScalePosition.x != ScalePosition.y)
-        {
-            ScalePosition.x = ScalePosition.x + (ScalePosition.y - ScalePosition.x) / BangDamping;
-        }
-
         var particles = new ParticleSystem.Particle[ParticleSystem.particleCount];
         ParticleSystem.GetParticles(particles);
 
         if (particles.Length <= 0) return;
+
+        if (Bang)
+        {
+            CurveIntensity.y = IntensityMultiplier;
+        }
+
+        if (CurveIntensity.x != CurveIntensity.y)
+        {
+            CurveIntensity.x = CurveIntensity.x + (CurveIntensity.y - CurveIntensity.x) / BangDamping;
+        }
 
         var subAngle = (2 * Mathf.PI) / particles.Length;
 
@@ -43,23 +45,34 @@ public class NoiseCircleController : MonoBehaviour
             var x = Mathf.Cos(angle) * Radius;
             var y = Mathf.Sin(angle) * Radius;
 
-            var scale = 1f;
-            var weight = 1f;
-            var offset = 0f;
+            var weight = WeightCurve.Evaluate((float)i / particles.Length);
 
-            if (i >= IndicesRange.x && i <= IndicesRange.y)
+            if (Bang)
             {
-                float subIndex = (i - IndicesRange.x) + 1;
-                float range = (IndicesRange.y - IndicesRange.x);
-                scale = ScalePosition.x;
-                weight = WeightCurve.Evaluate(subIndex / range);
-                offset = scale * weight;
+                if (CurveNoise)
+                {
+                    if (CurveNoiseValues == null || CurveNoiseValues.Length != particles.Length)
+                    {
+                        CurveNoiseValues = new float[particles.Length];
+                    }
+                    CurveNoiseValues[i] = 1 + (Mathf.PerlinNoise(x, y) * CurveNoiseIntensity);
+                }
             }
 
-            particles[i].position = new Vector2(x + offset, y + offset);
+            float noiseScale = 1;
+
+            if (CurveNoiseValues != null && CurveNoise)
+            {
+                noiseScale = CurveNoiseValues[i];
+            }
+
+            var curveOffset = CurveIntensity.x * weight;
+
+            particles[i].position = new Vector2(x + (x * (curveOffset * noiseScale)), y + (y * (curveOffset * noiseScale)));
         }
 
         ParticleSystem.SetParticles(particles, particles.Length);
 
+        Bang = false;
     }
 }
