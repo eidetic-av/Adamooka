d6922130cf162d97e1efb5b5e7dd9311b04f8eb2
Matthew Hughes <matt.hughes@live.com>
2019-02-12 14:48:59 +0100

    Started Guthman work

--- Assets/Scripts/RodParticleController.cs
+++ Assets/Scripts/RodParticleController.cs
@@ -11,10 +11,6 @@ public class RodParticleController : MonoBehaviour
 {
     public static RodParticleController Instance;
 
-    public GameObject ParticlesObject;
-    public ParticleSystem ParticleSystem;
-    private Renderer Renderer;
-
     public bool ConstantEmission = false;
     public bool EmissionEnabled = false;
 
@@ -26,120 +22,65 @@ public class RodParticleController : MonoBehaviour
     public Vector3 LeftMultiplier = new Vector3(0, 0, 0);
     public Vector3 RightMultiplier = new Vector3(0, 0, 0);
 
-    private AirSticks.Hand TrackingHand = AirSticks.Hand.Left;
     public int ParticleEmissionCount = 3;
 
-    public Vector3 BaseHandScale = new Vector3(0.1f, 0.1f, 0.1f);
-
-    public bool AirsticksScalingEnabled = true;
+    public Vector3 ScaleMultiplier = new Vector3(0.1f, 0.1f, 0.1f);
     public Vector2 AirsticksScaleMapX;
     public Vector2 AirsticksScaleMapY;
     public Vector2 AirsticksScaleMapZ;
 
     public Vector2 AirsticksRotationMinMax = new Vector2(0, 1);
 
-    public Color NewParticleColor = Color.white;
-    public bool SetColor = false;
-
-    public bool ContainWithinCircle = false;
-    public float CircleDistanceMultiplier = 1f;
-
     void Start()
     {
         Instance = this;
-        AirSticks.Right.NoteOn += () => EnableEmission();
-        AirSticks.Right.NoteOff += () => DisableEmission();
-
-        Renderer = ParticleSystem.GetComponent<Renderer>();
-    }
-
-    void EnableEmission()
-    {
-        EmissionEnabled = true;
-    }
-
-    void DisableEmission()
-    {
-        EmissionEnabled = false;
+        AirSticks.Right.NoteOn += () => EmissionEnabled = true;
+        AirSticks.Right.NoteOff += () => EmissionEnabled = false;
     }
 
     void Update()
     {
-        if (SetColor)
-        {
-            Renderer.material.SetColor("_Color", NewParticleColor);
-            SetColor = false;
-        }
-
-        if (ConstantEmission) EmissionEnabled = true;
         if (TrackAirsticks)
         {
-            Vector3 modulatedScale = BaseHandScale;
+            Vector3 scale = ScaleMultiplier;
 
-            Vector3 pos = Vector3.zero;
+            Vector3 position = Vector3.zero;
 
-            switch (TrackingHand)
+            if (Time.frameCount % 2 == 0)
             {
-                case AirSticks.Hand.Left:
-                    {
-                        // move to hand position
-                        pos = ParticlesObject.transform.position = new Vector3(
-                            (-AirSticks.Left.Position.x * LeftMultiplier.x) + LeftHandOffset.x,
-                            (AirSticks.Left.Position.y * LeftMultiplier.y) + LeftHandOffset.y,
-                            (AirSticks.Left.Position.z * LeftMultiplier.z) + LeftHandOffset.z);
-                        // set shape scale
-                        var mappedX = AirSticks.Left.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapX.x, AirsticksScaleMapX.y);
-                        var mappedY = AirSticks.Left.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapY.x, AirsticksScaleMapY.y);
-                        var mappedZ = AirSticks.Left.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapZ.x, AirsticksScaleMapZ.y);
-                        modulatedScale.Scale(new Vector3(mappedX, mappedY, mappedZ));
-                        // and swap to other hand for next frame
-                        TrackingHand = AirSticks.Hand.Right;
-                        break;
-                    }
-                case AirSticks.Hand.Right:
-                    {
-                        pos = ParticlesObject.transform.position = new Vector3(
-                            (-AirSticks.Right.Position.x * RightMultiplier.x) + RightHandOffset.x,
-                            (AirSticks.Right.Position.y * RightMultiplier.y) + RightHandOffset.y,
-                            (AirSticks.Right.Position.z * RightMultiplier.z) + RightHandOffset.z);
-                        // set shape scale
-                        var mappedX = AirSticks.Right.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapX.x, AirsticksScaleMapX.y);
-                        var mappedY = AirSticks.Right.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapY.x, AirsticksScaleMapY.y);
-                        var mappedZ = AirSticks.Right.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapZ.x, AirsticksScaleMapZ.y);
-                        modulatedScale.Scale(new Vector3(mappedX, mappedY, mappedZ));
-                        TrackingHand = AirSticks.Hand.Left;
-                        break;
-                    }
-            }
-
-            if (ContainWithinCircle)
-            {
-                var scaledPos = new Vector2(pos.x * CircleDistanceMultiplier, pos.y * CircleDistanceMultiplier);
-                var distance = Mathf.Sqrt(Mathf.Pow(scaledPos.x, 2) + Mathf.Pow(scaledPos.y, 2));
-                var radius = MembraneController.Instance.Radius.x;
-
-                if (distance > radius)
-                {
-                    var positionAlongLine = radius / distance;
-                    pos.x = positionAlongLine * (pos.x); 
-                    pos.y = positionAlongLine * (pos.y);
-                    ParticlesObject.transform.position = pos;
-                }
-            }
-
-            // Set the scale of the emission shape
-            var shapeModule = ParticleSystem.shape;
-            if (AirsticksScalingEnabled)
-            {
-                shapeModule.scale = modulatedScale;
+                position = transform.position = new Vector3(
+                    (-AirSticks.Left.Position.x * LeftMultiplier.x) + LeftHandOffset.x,
+                    (AirSticks.Left.Position.y * LeftMultiplier.y) + LeftHandOffset.y,
+                    (AirSticks.Left.Position.z * LeftMultiplier.z) + LeftHandOffset.z);
+                    
+                var mappedX = AirSticks.Left.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapX.x, AirsticksScaleMapX.y);
+                var mappedY = AirSticks.Left.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapY.x, AirsticksScaleMapY.y);
+                var mappedZ = AirSticks.Left.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapZ.x, AirsticksScaleMapZ.y);
+                scale.Scale(new Vector3(mappedX, mappedY, mappedZ));
             }
             else
             {
-                shapeModule.scale = BaseHandScale;
+                position = transform.position = new Vector3(
+                    (-AirSticks.Right.Position.x * RightMultiplier.x) + RightHandOffset.x,
+                    (AirSticks.Right.Position.y * RightMultiplier.y) + RightHandOffset.y,
+                    (AirSticks.Right.Position.z * RightMultiplier.z) + RightHandOffset.z);
+
+                var mappedX = AirSticks.Right.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapX.x, AirsticksScaleMapX.y);
+                var mappedY = AirSticks.Right.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapY.x, AirsticksScaleMapY.y);
+                var mappedZ = AirSticks.Right.EulerAngles.z.Map(AirsticksRotationMinMax.x, AirsticksRotationMinMax.y, AirsticksScaleMapZ.x, AirsticksScaleMapZ.y);
+                scale.Scale(new Vector3(mappedX, mappedY, mappedZ));
             }
 
-            if (EmissionEnabled) ParticleSystem.Emit(ParticleEmissionCount);
-
+            var shapeModule = GetComponent<ParticleSystem>().shape;
+            shapeModule.scale = scale;
         }
+        else
+        {
+            var shapeModule = GetComponent<ParticleSystem>().shape;
+            shapeModule.scale = ScaleMultiplier;
+        }
+
+        if (EmissionEnabled || ConstantEmission)
+            GetComponent<ParticleSystem>().Emit(ParticleEmissionCount);
     }
 }
