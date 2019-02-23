3bed8565729c946ed1bfbf05633ccbd5782c5c6b
Matthew Hughes <matt.hughes@live.com>
2019-02-23 18:20:43 +0100

    Started work with RuntimeInspector to allow Alon to change mapping values himself after compile

---
+++ Assets/ParticleSystemFollower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleSystemFollower : MonoBehaviour
+{
+	public int ParticleCount;
+    public List<ParticleSystem> ParticleSystems;
+
+    void Update()
+    {
+        ParticleCount = ParticleSystems.Sum(p => p.particleCount);
+		if (ParticleCount <= 0) return;
+
+		GetComponent<ParticleSystem>().Clear();
+        GetComponent<ParticleSystem>().SetParticles(
+            ParticleSystems.SelectMany(system =>
+                {
+                    var theseParticles = new ParticleSystem.Particle[system.particleCount];
+                    system.GetParticles(theseParticles);
+					for (int i = 0; i < theseParticles.Length; i++)
+						theseParticles[i].position += system.transform.position;
+                    return theseParticles;
+                }).ToArray(),
+            ParticleCount
+        );
+    }
+}
---
+++ Assets/RodController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodController : MonoBehaviour
+{
+	public float Test;
+    ParticleSystem LeftHand;
+    ParticleSystem RightHand;
+    ParticleSystem Ribbon;
+    // Use this for initialization
+    void Start()
+    {
+        LeftHand = GameObject.Find("LeftHand").GetComponent<ParticleSystem>();
+        RightHand = GameObject.Find("RightHand").GetComponent<ParticleSystem>();
+        Ribbon = GameObject.Find("Ribbon").GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckKeyboardInput(LeftHand, KeyCode.LeftArrow);
+        CheckKeyboardInput(RightHand, KeyCode.RightArrow);
+    }
+
+    void CheckKeyboardInput(ParticleSystem particleSystem, KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+			{
+                particleSystem.Clear();
+				particleSystem.Play();
+				GameObject.Find("RuntimeInspector")
+					.GetComponent<RuntimeInspectorNamespace.RuntimeInspector>()
+					.Inspect(GameObject.Find("Rods"));
+			}
+        }
+    }
+}
