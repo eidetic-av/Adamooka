69e263ad1b40b5eacb1d3b378655928b8600da1c
Matthew Hughes <matt.hughes@live.com>
2019-02-23 19:58:04 +0100

    Updated RodController to work with property changes at runtime

--- Assets/RodController.cs
+++ Assets/RodController.cs
@@ -4,37 +4,46 @@ using UnityEngine;
 
 public class RodController : MonoBehaviour
 {
-	public float Test;
+
     ParticleSystem LeftHand;
     ParticleSystem RightHand;
     ParticleSystem Ribbon;
-    // Use this for initialization
-    void Start()
+	void Start()
     {
         LeftHand = GameObject.Find("LeftHand").GetComponent<ParticleSystem>();
         RightHand = GameObject.Find("RightHand").GetComponent<ParticleSystem>();
         Ribbon = GameObject.Find("Ribbon").GetComponent<ParticleSystem>();
     }
 
+    // Control properties at runtime
+    [SerializeField]
+    float _test;
+    public float Test
+    {
+        get { return _test; }
+        set
+        {
+			LeftHand.gameObject.SetActive(value > 5);
+			_test = value;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        CheckKeyboardInput(LeftHand, KeyCode.LeftArrow);
-        CheckKeyboardInput(RightHand, KeyCode.RightArrow);
+        CheckKeyboardInput(LeftHand, KeyCode.LeftArrow, true);
+        CheckKeyboardInput(RightHand, KeyCode.RightArrow, true);
     }
 
-    void CheckKeyboardInput(ParticleSystem particleSystem, KeyCode key)
+    void CheckKeyboardInput(ParticleSystem particleSystem, KeyCode key, bool requiresShift = false)
     {
         if (Input.GetKeyDown(key))
         {
-            if (Input.GetKey(KeyCode.LeftShift))
-			{
+            if (Input.GetKey(KeyCode.LeftShift) || !requiresShift)
+            {
                 particleSystem.Clear();
-				particleSystem.Play();
-				GameObject.Find("RuntimeInspector")
-					.GetComponent<RuntimeInspectorNamespace.RuntimeInspector>()
-					.Inspect(GameObject.Find("Rods"));
-			}
+                particleSystem.Play();
+            }
         }
     }
 }
---
+++ Assets/RuntimeEditorInputControl.cs
@@ -0,0 +1,43 @@
+﻿using RuntimeInspectorNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RuntimeEditorInputControl : MonoBehaviour
+{
+    public static RuntimeInspector Inspector;
+    public static bool Active;
+
+	public bool ActiveOnAwake;
+    public List<GameObject> FocusedObjects;
+
+    void Awake()
+    {
+        Inspector = GameObject.Find("RuntimeInspector")
+            .GetComponent<RuntimeInspector>();
+		Inspector.gameObject.SetActive(ActiveOnAwake);
+    }
+
+    void Update()
+    {
+        // Toggle inspector with 'I' key.
+        if (Input.GetKeyDown(KeyCode.I))
+            Inspector.gameObject.SetActive(Active = !Active);
+        else if (Active)
+        {
+            // Set inspector focus to the index(+1) selected with alpha keys
+            FocusedObjects.FirstOrDefault(o =>
+                Input.GetKeyDown((FocusedObjects.IndexOf(o) + 1).ToString()))?
+                .Focus();
+        }
+    }
+}
+
+public static class RuntimeEditorExtensionMethods
+{
+    public static void Focus(this GameObject gameObject)
+    {
+        RuntimeEditorInputControl.Inspector.Inspect(gameObject);
+    }
+}
