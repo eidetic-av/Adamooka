4fa35fb8cdeb8c28e46644f16c548aa1f694b0df
Matthew Hughes <matt.hughes@live.com>
2019-02-25 00:53:04 +0100

    Started moving over Ring Sampler to Guthman scene

---
+++ Assets/Scripts/Andamooka/RingController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using RuntimeInspectorNamespace;
+using Eidetic.Andamooka;
+using Eidetic.Unity.Runtime;
+using Eidetic.Unity.Utility;
+
+[System.Serializable]
+public class RingController : RuntimeController
+{
+
+    //
+    // Runtime control properties
+    //
+    public bool ControlWithAirSticks {get; set;} = true;
+
+    //
+    // Initialisation
+    //
+    ParticleSystem InnerRing;
+    ParticleSystem Membrane;
+    void Start()
+    {
+        InnerRing = GameObject.Find("NoiseCircleSystem")
+            .GetComponent<ParticleSystem>();
+        Membrane = GameObject.Find("MembraneSystem")
+            .GetComponent<ParticleSystem>();
+    }
+
+    //
+    // Runtime loop
+    //
+    void Update() 
+    {
+        if (ControlWithAirSticks)
+        {
+                
+        }
+    }
+
+    //
+    // Inner methods
+    //
+}
